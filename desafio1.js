1dc590a31f79483849209a3e08f97e1490c9284a
eliudarango <eliudmontiel@outlook.com>
2025-01-24 21:40:15 -0600

    v2

--- desafio1.js
+++ desafio1.js
@@ -1,28 +1,38 @@
 //Desarrollador: Eliud Arango
 //24/01/2025
-
+//-------------------------------------------------------------------------------------------------
 /*Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".*/
 alert("Bienvenida y bienvenido a nuestro sitio web");
+//-------------------------------------------------------------------------------------------------
 /*Declara una variable llamada nombre y asígnale el valor "Lua".*/
 let nombre = "Lua";
+//-------------------------------------------------------------------------------------------------
 /*Crea una variable llamada edady asígnale el valor 25.*/
 let edad = 25;
+//-------------------------------------------------------------------------------------------------
 /*Establece una variable numeroDeVentas y asígnale el valor 50.*/
 let numeroVentas = 50;
+//-------------------------------------------------------------------------------------------------
 /*Establece una variable saldoDisponible y asígnale el valor 1000.*/
 let saldoDisponible = 1000;
+//-------------------------------------------------------------------------------------------------
 /*Muestra una alerta con el texto "¡Error! Completa todos los campos".*/
 alert ("¡Error! Completa todos los campos");
+//-------------------------------------------------------------------------------------------------
 /*Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! 
 Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .*/
 let mensajeDeError = "Error! Completa todos los campos";
 alert(mensajeDeError);
+//-------------------------------------------------------------------------------------------------
 /*Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.*/
 let nombre1 = prompt("Cual es su nombre");
+//-------------------------------------------------------------------------------------------------
 /*Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.*/
 let edad1 = prompt("Cual es su edad");
+//-------------------------------------------------------------------------------------------------
 /*Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu 
 licencia de conducir!".Si necesitas ayuda, las soluciones a las actividades estarán disponibles en la sección*/
 if(edad1 >= 18){
 alert("Puedes obtener tu licencia de conducir!");
 }
+//-------------------------------------------------------------------------------------------------
